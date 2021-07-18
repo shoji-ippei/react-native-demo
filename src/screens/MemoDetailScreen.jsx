b6ebeec615c28c9826240e836af2b7a5bbde1ed7
shoji-ippei <ippei-shoji@cuebic.co.jp>
2021-07-18 15:41:51 +0900

    feat: memo details

---
+++ src/screens/MemoDetailScreen.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
+
+import AppBar from '../components/AppBar';
+import CircleButton from '../components/CircleButton';
+
+export default function MemoDetailScreen() {
+  return (
+    <View style={styles.container}>
+      <AppBar />
+      <View style={styles.memoheader}>
+        <Text style={styles.memoTitle}>買い物リスト</Text>
+        <Text style={styles.memoDate}>2021年7月18日 10:00:00</Text>
+      </View>
+      <ScrollView style={styles.memoBody}>
+        <Text style={styles.memoText}>
+          買い物リスト
+          ほげ
+          ほげ
+        </Text>
+      </ScrollView>
+      <CircleButton style={{ top: 160, bottom: 'auto' }}>+</CircleButton>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  memoheader: {
+    backgroundColor: '#467FD3',
+    height: 96,
+    justifyContent: 'center',
+    paddingVertical: 24,
+    paddingHorizontal: 19,
+  },
+  memoTitle: {
+    color: '#ffffff',
+    fontSize: 28,
+    lineHeight: 32,
+    fontWeight: 'bold',
+  },
+  memoDate: {
+    color: '#ffffff',
+    fontSize: 12,
+    lineHeight: 16,
+  },
+  memoBody: {
+    paddingVertical: 32,
+    paddingHorizontal: 27,
+  },
+  memoText: {
+    fontSize: 16,
+    lineHeight: 24,
+  },
+});
