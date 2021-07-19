2a1f66cbc308b6d891ca231ebdc7244d0206b283
shoji-ippei <ippei-shoji@cuebic.co.jp>
2021-07-19 22:46:02 +0900

    feat: replace keyboardAvoidingView

--- App.jsx
+++ App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 // import MemoListScreen from './src/screens/MemoListScreen';
-// import MemoEditScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MemoCreateScreen';
+import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoCreateScreen from './src/screens/MemoCreateScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 
 export default function App() {
   return (
-    <MemoCreateScreen />
+    <MemoEditScreen />
   );
 }
