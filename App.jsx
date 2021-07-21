646b4743f81342e6b98790c748e57527b62deb62
shoji-ippei <ippei-shoji@cuebic.co.jp>
2021-07-21 21:53:08 +0900

    feat: login/signup screen

--- App.jsx
+++ App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 // import MemoListScreen from './src/screens/MemoListScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import LogInScreen from './src/screens/LogInScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
   return (
-    <MemoEditScreen />
+    <SignUpScreen />
   );
 }
